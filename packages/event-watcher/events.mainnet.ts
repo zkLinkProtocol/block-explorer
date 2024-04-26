87aab8c156f2a5de66bab742a68ac22baa62128e
yukinota <yuke@zklink.org>
2024-04-26 16:28:25 +0800

    dev batchevent decode

--- packages/event-watcher/events.mainnet.ts
+++ packages/event-watcher/events.mainnet.ts
@@ -45,7 +45,7 @@ export const EVENTS_MAINNET = {
           contractDeploymentBlock: 1505514,
         },
       ],
-      // optim
+      // op
       '10': [
         {
           contractAddress: '0x46C8D02E93d5a03899dFa7Cf8A40A07589A3fA1b',
@@ -57,6 +57,68 @@ export const EVENTS_MAINNET = {
         {
           contractAddress: '0xE473ce141b1416Fe526eb63Cf7433b7B8d7264Dd',
           contractDeploymentBlock: 1505514,
+        },
+      ],
+    },
+  },
+  OpenRangeBatchRoot: {
+    name: 'OpenRangeBatchRoot',
+    topic: '0xddf6b2cf5bdb04a03cb3f7663b72571336626b1e586a3a047942e4860cf7e12b',
+    chains: {
+      // eth
+      '1': [
+        {
+          contractAddress: '0x5fD9F73286b7E8683Bab45019C94553b93e015Cf',
+          contractDeploymentBlock: 19696592,//do
+        },
+      ],
+      // zks
+      '324': [
+        {
+          contractAddress: '0xaFe8C7Cf33eD0fee179DFF20ae174C660883273A',
+          contractDeploymentBlock: 28518167,
+        },
+      ],
+      // arb
+      '42161': [
+        {
+          contractAddress: '0xFF73a1a1d27951A005eb23276dc99CB7F8d5420A',
+          contractDeploymentBlock: 203514238,//do
+        },
+      ],
+      // mantle
+      '5000': [
+        {
+          contractAddress: '0xD784d7128B46B60Ca7d8BdC17dCEC94917455657',
+          contractDeploymentBlock: 60103171,
+        },
+      ],
+      // manta
+      '169': [
+        {
+          contractAddress: '0xD784d7128B46B60Ca7d8BdC17dCEC94917455657',
+          contractDeploymentBlock: 1575547,
+        },
+      ],
+      // blast
+      '81457': [
+        {
+          contractAddress: '0x29BA92Fe724beD5c5EBfd0099F2F64a6DC5078FD',
+          contractDeploymentBlock: 2152208,
+        },
+      ],
+      // op
+      '10': [
+        {
+          contractAddress: '0x46C8D02E93d5a03899dFa7Cf8A40A07589A3fA1b',
+          contractDeploymentBlock: 118923858,
+        },
+      ],
+      // base
+      '8453': [
+        {
+          contractAddress: '0xE473ce141b1416Fe526eb63Cf7433b7B8d7264Dd',
+          contractDeploymentBlock: 13328673,
         },
       ],
     },
