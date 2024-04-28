b32b10761d7bcfe2d46fda15ddc4897be7a9e4c6
yukinota <yuke@zklink.org>
2024-04-28 15:20:55 +0800

    add new chainId config and sepolia config

--- packages/event-watcher/events.mainnet.ts
+++ packages/event-watcher/events.mainnet.ts
@@ -3,34 +3,60 @@ export const EVENTS_MAINNET = {
     name: 'SyncBatchRoot',
     topic: '0xab53214e3a56f049923c5f5532ea4657bba51b870850593c4dc0eceb3b61feed',
     chains: {
+      // eth
       '1': [
         {
           contractAddress: '0x5fD9F73286b7E8683Bab45019C94553b93e015Cf',
           contractDeploymentBlock: 19338891,
         },
       ],
+      // zks
       '324': [
         {
           contractAddress: '0xaFe8C7Cf33eD0fee179DFF20ae174C660883273A',
           contractDeploymentBlock: 27849895,
         },
       ],
+      // arb
       '42161': [
         {
           contractAddress: '0xFF73a1a1d27951A005eb23276dc99CB7F8d5420A',
           contractDeploymentBlock: 185950340,
         },
       ],
+      // mantle
       '5000': [
         {
           contractAddress: '0xD784d7128B46B60Ca7d8BdC17dCEC94917455657',
           contractDeploymentBlock: 58659816,
         },
       ],
+      // manta
       '169': [
         {
           contractAddress: '0xD784d7128B46B60Ca7d8BdC17dCEC94917455657',
           contractDeploymentBlock: 1505514,
+        },
+      ],
+      // blast
+      '81457': [
+        {
+          contractAddress: '0x29BA92Fe724beD5c5EBfd0099F2F64a6DC5078FD',
+          contractDeploymentBlock: 803957,
+        },
+      ],
+      // optim
+      '10': [
+        {
+          contractAddress: '0x46C8D02E93d5a03899dFa7Cf8A40A07589A3fA1b',
+          contractDeploymentBlock: 117746854,
+        },
+      ],
+      // base
+      '8453': [
+        {
+          contractAddress: '0xE473ce141b1416Fe526eb63Cf7433b7B8d7264Dd',
+          contractDeploymentBlock: 12153175,
         },
       ],
     },
---
+++ packages/event-watcher/events.sepolia.ts
@@ -0,0 +1,15 @@
+export const EVENTS_SEPOLIA = {
+    SyncBatchRoot: {
+        name: 'SyncBatchRoot',
+        topic: '0xab53214e3a56f049923c5f5532ea4657bba51b870850593c4dc0eceb3b61feed',
+        chains: {
+            // sepolia
+            '11155111': [
+                {
+                    contractAddress: '0x5fD9F73286b7E8683Bab45019C94553b93e015Cf',
+                    contractDeploymentBlock: 19338891,
+                },
+            ],
+        },
+    },
+};
