fa25cdf45adcd5c5c11b3f5ff4dceae9ecc6f5d5
nickwest <nickwest@zklink.org>
2024-04-29 19:53:25 -0700

    fix lint

--- packages/worker/src/migrations/1714388505242-AddIneligibleAddress.ts
+++ packages/worker/src/migrations/1714388505242-AddIneligibleAddress.ts
@@ -1,14 +1,15 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class AddIneligibleAddress1714388505242 implements MigrationInterface {
-    name = 'AddIneligibleAddress1714388505242'
+  name = "AddIneligibleAddress1714388505242";
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE "ineligibleAddresses" ("address" bytea NOT NULL, "dappName" character varying NOT NULL, CONSTRAINT "PK_974e9f649b2ff70c78dd70e5e39" PRIMARY KEY ("address"))`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE "ineligibleAddresses" ("address" bytea NOT NULL, "dappName" character varying NOT NULL, CONSTRAINT "PK_974e9f649b2ff70c78dd70e5e39" PRIMARY KEY ("address"))`
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`DROP TABLE "ineligibleAddresses"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP TABLE "ineligibleAddresses"`);
+  }
 }
