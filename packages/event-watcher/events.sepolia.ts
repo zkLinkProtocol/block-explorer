2a5f56b490e9b6d2f810a49a187aa340fb43dd5c
yukinota <yuke@zklink.org>
2024-05-02 15:40:28 +0800

    update events.sepolia.ts

--- packages/event-watcher/events.sepolia.ts
+++ packages/event-watcher/events.sepolia.ts
@@ -1,55 +1,13 @@
-export const EVENTS_MAINNET = {
+export const EVENTS_SEPOLIA = {
   SyncBatchRoot: {
     name: 'SyncBatchRoot',
     topic: '0xab53214e3a56f049923c5f5532ea4657bba51b870850593c4dc0eceb3b61feed',
     chains: {
-      // eth
-      '1': [
+      // sepolia
+      '11155111': [
         {
-          contractAddress: '0x9719cD314BBf84B18aAEDEF56DF88E2267aA01e3',
+          contractAddress: '0x5fD9F73286b7E8683Bab45019C94553b93e015Cf',
           contractDeploymentBlock: 19338891,
-        },
-      ],
-      // zks
-      '324': [
-        {
-          contractAddress: '0x02627EFACfc2B000E77132fE9346C543eB980bAb',
-          contractDeploymentBlock: 27849895,
-        },
-      ],
-      // arb
-      '42161': [
-        {
-          contractAddress: '0xaE1875112Ae010A9fe755418B206AfB33Ee0b1fA',
-          contractDeploymentBlock: 185950340,
-        },
-      ],
-      // manta
-      '169': [
-        {
-          contractAddress: '0xEe5aFbd53D661968d13315f6960BBb103C2a1eCc',
-          contractDeploymentBlock: 1505514,
-        },
-      ],
-      // blast
-      '81457': [
-        {
-          contractAddress: '0x27CBbE82447a0C188eBD7Bc5fd706d140c7B0642',
-          contractDeploymentBlock: 1505514,
-        },
-      ],
-      // optim
-      '10': [
-        {
-          contractAddress: '0xbaC8EF345C684B0871dF390f44273160Ba3E6bc1',
-          contractDeploymentBlock: 1505514,
-        },
-      ],
-      // base
-      '8453': [
-        {
-          contractAddress: '0x8c4b80A5D5374Ff2Dc07310EF9Fdbc44e487b6C2',
-          contractDeploymentBlock: 1505514,
         },
       ],
     },
