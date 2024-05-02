c214008c063f71f7f2908aa99056bb14c4120e53
yukinota <yuke@zklink.org>
2024-05-02 15:34:48 +0800

    update event name and topic and block number

--- packages/event-watcher/events.mainnet.ts
+++ packages/event-watcher/events.mainnet.ts
@@ -1,66 +1,4 @@
 export const EVENTS_MAINNET = {
-  SyncBatchRoot: {
-    name: 'SyncBatchRoot',
-    topic: '0xab53214e3a56f049923c5f5532ea4657bba51b870850593c4dc0eceb3b61feed',
-    chains: {
-      // eth
-      '1': [
-        {
-          contractAddress: '0x5fD9F73286b7E8683Bab45019C94553b93e015Cf',
-          contractDeploymentBlock: 19338891,
-        },
-      ],
-      // zks
-      '324': [
-        {
-          contractAddress: '0xaFe8C7Cf33eD0fee179DFF20ae174C660883273A',
-          contractDeploymentBlock: 27849895,
-        },
-      ],
-      // arb
-      '42161': [
-        {
-          contractAddress: '0xFF73a1a1d27951A005eb23276dc99CB7F8d5420A',
-          contractDeploymentBlock: 185950340,
-        },
-      ],
-      // mantle
-      '5000': [
-        {
-          contractAddress: '0xD784d7128B46B60Ca7d8BdC17dCEC94917455657',
-          contractDeploymentBlock: 58659816,
-        },
-      ],
-      // manta
-      '169': [
-        {
-          contractAddress: '0xD784d7128B46B60Ca7d8BdC17dCEC94917455657',
-          contractDeploymentBlock: 1505514,
-        },
-      ],
-      // blast
-      '81457': [
-        {
-          contractAddress: '0x29BA92Fe724beD5c5EBfd0099F2F64a6DC5078FD',
-          contractDeploymentBlock: 803957,
-        },
-      ],
-      // op
-      '10': [
-        {
-          contractAddress: '0x46C8D02E93d5a03899dFa7Cf8A40A07589A3fA1b',
-          contractDeploymentBlock: 117746854,
-        },
-      ],
-      // base
-      '8453': [
-        {
-          contractAddress: '0xE473ce141b1416Fe526eb63Cf7433b7B8d7264Dd',
-          contractDeploymentBlock: 12153175,
-        },
-      ],
-    },
-  },
   OpenRangeBatchRoot: {
     name: 'OpenRangeBatchRoot',
     topic: '0xddf6b2cf5bdb04a03cb3f7663b72571336626b1e586a3a047942e4860cf7e12b',
@@ -69,7 +7,7 @@ export const EVENTS_MAINNET = {
       '1': [
         {
           contractAddress: '0x5fD9F73286b7E8683Bab45019C94553b93e015Cf',
-          contractDeploymentBlock: 19696592,//do
+          contractDeploymentBlock: 19696592,
         },
       ],
       // zks
@@ -83,14 +21,14 @@ export const EVENTS_MAINNET = {
       '42161': [
         {
           contractAddress: '0xFF73a1a1d27951A005eb23276dc99CB7F8d5420A',
-          contractDeploymentBlock: 203514238,//do
+          contractDeploymentBlock: 203514238,
         },
       ],
       // mantle
       '5000': [
         {
           contractAddress: '0xD784d7128B46B60Ca7d8BdC17dCEC94917455657',
-          contractDeploymentBlock: 60103171,
+          contractDeploymentBlock: 62614031,
         },
       ],
       // manta
@@ -104,10 +42,10 @@ export const EVENTS_MAINNET = {
       '81457': [
         {
           contractAddress: '0x29BA92Fe724beD5c5EBfd0099F2F64a6DC5078FD',
-          contractDeploymentBlock: 2152208,
+          contractDeploymentBlock: 2471615,
         },
       ],
-      // op
+      // optimistic
       '10': [
         {
           contractAddress: '0x46C8D02E93d5a03899dFa7Cf8A40A07589A3fA1b',
