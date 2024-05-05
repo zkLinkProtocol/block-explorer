8925c3ac1cb79814a22cdf14ea3579c7146c0e60
yukinota <yuke@zklink.org>
2024-05-05 22:55:12 +0800

    rename res value

--- packages/worker/src/repositories/transfer.repository.ts
+++ packages/worker/src/repositories/transfer.repository.ts
@@ -62,9 +62,9 @@ export class TransferRepository extends BaseRepository<Transfer> {
       AND transfers.timestamp >= $2
       AND transfers."tokenAddress" = $3
     `, [TransferType.Withdrawal, sevenDaysAgo, tokenAddress]);
-    if (res[0].totalAmount === null || res[0].totalAmount === undefined) {
+    if (res[0].totalamount === null || res[0].totalamount === undefined) {
       return BigNumber.from(0);
     }
-    return BigNumber.from(res[0].totalAmount);
+    return BigNumber.from(res[0].totalamount);
   }
 }
